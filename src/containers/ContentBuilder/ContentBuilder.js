ca58b928afd6165e50578d78fc236c286587a23a
Joshua Corpin <jkcorpin@gmail.com>
2020-07-29 17:26:47 +0800

    Convert to React Hooks

--- src/containers/ContentBuilder/ContentBuilder.js
+++ src/containers/ContentBuilder/ContentBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Contents from "../../components/Contents/Contents";
 import Spinner from "../../components/UI/Spinner/Spinner";
@@ -6,54 +6,48 @@ import Pagination from "../../components/UI/Pagination/Pagination";
 import Aux from "../../hoc/Auxilliary/Auxilliary";
 import classes from "./ContentBuilder.module.css";
 
-class ContentBuilder extends Component {
-  state = {
-    contents: [],
-    loading: false,
-    currentPage: 1,
-    postsPerPage: 15,
-  };
+const ContentBuilder = (props) => {
+  const [contents, setContents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(15);
 
-  async componentDidMount() {
-    try {
-      this.setState({ loading: true });
-      const contents = await axios.get("/result.json");
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPost = contents.slice(indexOfFirstPost, indexOfLastPost);
 
-      this.setState({
-        contents: contents.data,
-        loading: false,
-      });
-    } catch (error) {
-      this.setState({ loading: true });
-    }
-  }
-
-  paginate = (pageNumber) => {
-    this.setState({ currentPage: pageNumber });
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
     window.scrollTo(0, 0);
   };
 
-  render() {
-    const { currentPage, postsPerPage, contents } = this.state;
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPost = contents.slice(indexOfFirstPost, indexOfLastPost);
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const contents = await axios.get("/result.json");
+        setContents(contents.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(true);
+      }
+    })();
+  }, []);
 
-    let contentBody = <Contents datas={currentPost} />;
-    if (this.state.loading) contentBody = <Spinner />;
+  let contentBody = <Contents datas={currentPost} />;
+  if (loading) contentBody = <Spinner />;
 
-    return (
-      <Aux>
-        <div className={`row ${classes.MarginContent}`}>{contentBody}</div>
-        <Pagination
-          postPerPage={this.state.postsPerPage}
-          totalPosts={this.state.contents.length}
-          paginate={this.paginate}
-          currentPage={this.state.currentPage}
-        />
-      </Aux>
-    );
-  }
-}
+  return (
+    <Aux>
+      <div className={`row ${classes.MarginContent}`}>{contentBody}</div>
+      <Pagination
+        postPerPage={postsPerPage}
+        totalPosts={contents.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
+    </Aux>
+  );
+};
 
 export default ContentBuilder;
