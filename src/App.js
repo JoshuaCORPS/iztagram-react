c13258c71f223d26172cfd98815c7aa365a19fb8
Joshua Corpin <jkcorpin@gmail.com>
2020-08-03 10:28:58 +0800

    Changed to functional component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import ContentBuilder from "./containers/ContentBuilder/ContentBuilder";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/" exact component={ContentBuilder} />
-            <Redirect from="*" to="/" />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  return (
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={ContentBuilder} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Layout>
+    </div>
+  );
+};
 
 export default App;
