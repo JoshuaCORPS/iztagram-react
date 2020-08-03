f2d98320fffb72947d644bfae65543788a5bfa0e
Joshua Corpin <jkcorpin@gmail.com>
2020-08-03 10:21:18 +0800

    Changed to functional component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import Aux from "../Auxilliary/Auxilliary";
 import Navigation from "../../components/UI/Navigation/Navigation";
 
-class Layout extends Component {
-  render() {
-    return (
-      <Aux>
-        <Navigation />
-        <main className="container">{this.props.children}</main>
-      </Aux>
-    );
-  }
-}
-
+const Layout = (props) => {
+  return (
+    <Aux>
+      <Navigation />
+      <main className="container">{props.children}</main>
+    </Aux>
+  );
+};
 export default Layout;
